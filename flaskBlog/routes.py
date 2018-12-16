e4063ffd66505c086456d58286bafd6a3f41d4e3
msunilhyd <msunilhyd@gmail.com>
2018-12-16 20:03:56 +0530

    16-Dec-18 : Removed the pagination to check the cache performance on cloud

--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -12,23 +12,20 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 def main():
 	page = request.args.get('page', 1, type=int)
-	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+	posts = Post.query.all()
 	return render_template('home.html', posts=posts)
 
 @app.route("/user/<string:username>")
 def user_posts(username):	
 	page = request.args.get('page', 1, type=int)
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = Post.query\
-				.filter_by(author=user)\
-				.order_by(Post.date_posted.desc())\
-				.paginate(page=page, per_page=5)
+	posts = Post.query.all()
 	return render_template('user_posts.html', posts=posts, user=user)
 
 @app.route("/home")
 def home():	
 	page = request.args.get('page', 1, type=int)
-	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+	posts = Post.query.all()
 	return render_template('home.html', posts=posts)
 
 
